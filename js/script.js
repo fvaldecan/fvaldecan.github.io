4e8e5c1ff38d20cab91be917dc55cb7a0ac10377
Franco Dominic Valdecanas <valdecan@sonoma.edu>
2021-01-09 18:54:18 -0800

    Change main pic and update resume

--- js/script.js
+++ js/script.js
@@ -1,80 +1,79 @@
 (function ($) {
-    'use strict';
-    
-AOS.init({
-    once: true
-});
-    $(".nav-link").click(function(){
-        var pageId = $(this).attr("data-page");
-        $("html, body").animate({ scrollTop: $("#"+pageId).offset().top }, 1000);
-        $(".navbar-collapse").removeClass("show");
-    });
-    // ----------------------- 
-        // Progress Bar--------------------
-        // 
-        // 
-    $(window).on ('load', function (){ 
-        $('.progress-bar').each(function(){
-                var width = $(this).data('percent');
-                $(this).css({'transition': 'width 3s'});
-                $(this).appear(function() {
-                    console.log('hello');
-                    $(this).css('width', width + '%');
-                    $(this).find('.count').countTo({
-                        from: 0,
-                        to: width,
-                        speed: 3000,
-                        refreshInterval: 50
-                    });
-                });
-            });
-        }
-    )
-    .on('scroll', function(){
-        let scrollTop = $(window).scrollTop(),
-        projectSection = $('#about').offset().top,
+  "use strict";
+
+  AOS.init({
+    once: true,
+  });
+  $(".nav-link").click(function () {
+    var pageId = $(this).attr("data-page");
+    $("html, body").animate({ scrollTop: $("#" + pageId).offset().top }, 1000);
+    $(".navbar-collapse").removeClass("show");
+  });
+  // -----------------------
+  // Progress Bar--------------------
+  //
+  //
+  $(window)
+    .on("load", function () {
+      $(".progress-bar").each(function () {
+        var width = $(this).data("percent");
+        $(this).css({ transition: "width 3s" });
+        $(this).appear(function () {
+          console.log("hello");
+          $(this).css("width", width + "%");
+          $(this).find(".count").countTo({
+            from: 0,
+            to: width,
+            speed: 3000,
+            refreshInterval: 50,
+          });
+        });
+      });
+    })
+    .on("scroll", function () {
+      let scrollTop = $(window).scrollTop(),
+        projectSection = $("#about").offset().top,
         navHeight = $(".navbar").height(),
         distanceTop = projectSection - scrollTop - navHeight;
-        if(distanceTop > 0) {
-            $('.navbar').removeClass('faded')
-        }
-        else if (distanceTop < 0) {
-            $('.navbar').addClass('faded')
-        }
+      if (distanceTop > 0) {
+        $(".navbar").removeClass("faded");
+      } else if (distanceTop < 0) {
+        $(".navbar").addClass("faded");
+      }
     });
 
-    $('.owl-carousel').owlCarousel({
-        items:1,
-        loop:true,
-        autoplay:true,
-        dots:false,
-        autoplayTimeout:8000
-    });
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    dots: false,
+    autoplayTimeout: 8000,
+  });
 
-    // Shuffle js filter and masonry
-    var Shuffle = window.Shuffle;
-    var jQuery = window.jQuery;
+  // Shuffle js filter and masonry
+  var Shuffle = window.Shuffle;
+  var jQuery = window.jQuery;
 
-    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-        itemSelector: '.shuffle-item',
-        buffer: 1
-    });
+  var myShuffle = new Shuffle(document.querySelector(".shuffle-wrapper"), {
+    itemSelector: ".shuffle-item",
+    buffer: 1,
+  });
 
-    jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-        var input = evt.currentTarget;
-        if (input.checked) {
-            myShuffle.filter(input.value);
-        }
-    });
+  jQuery('input[name="shuffle-filter"]').on("change", function (evt) {
+    var input = evt.currentTarget;
+    if (input.checked) {
+      myShuffle.filter(input.value);
+    }
+  });
 
-     $('.portfolio-gallery').each(function () {
-        $(this).find('.popup-gallery').magnificPopup({
-            type: 'image',
-            gallery: {
-                enabled: true
-            }
-        });
-    });
-
-
+  $(".portfolio-gallery").each(function () {
+    $(this)
+      .find(".popup-gallery")
+      .magnificPopup({
+        type: "image",
+        gallery: {
+          enabled: true,
+        },
+      });
+  });
 })(jQuery);
